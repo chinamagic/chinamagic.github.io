97843672f85e3f0a0bb9f1913d67f770ef70f365
chinamagic <44197851+chinamagic@users.noreply.github.com>
2020-03-10 22:01:40 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2bca0452f445a54794e9a4289b09da6b.js"
+  "/precache-manifest.e3aacce5006c25ab8ed75ae76fd15df1.js"
 );
 
 workbox.clientsClaim();
